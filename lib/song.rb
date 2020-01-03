3511be25e2e95945ffc91e5e0ab8f7833a81dd9d
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-03 00:18:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song 
   attr_accessor :title,:artist 
   
+  # def initialize 
+  #   @title = title
+  #   @author = author 
+  # end 
+  
   def self.artist  
     self.Artist  = artist 
   end 
